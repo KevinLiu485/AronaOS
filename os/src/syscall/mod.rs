29a78695c84715d2b1e7ce5d573e77eea1435996
Kevin <2424539083@qq.com>
2024-05-29 12:15:14 +0800

    Temp save

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -9,6 +9,10 @@
 //! For clarity, each single syscall is implemented as its own function, named
 //! `sys_` then the name of the syscall. You can find functions like this in
 //! submodules, and you should also implement syscalls this way.
+
+#![allow(unused)]
+#![allow(non_upper_case_globals)]
+
 const SYSCALL_OPEN: usize = 56;
 const SYSCALL_CLOSE: usize = 57;
 const SYSCALL_READ: usize = 63;
@@ -20,6 +24,40 @@ const SYSCALL_GETPID: usize = 172;
 const SYSCALL_FORK: usize = 220;
 const SYSCALL_EXEC: usize = 221;
 const SYSCALL_WAITPID: usize = 260;
+
+// os-comp testsuits
+const SYS_getcwd: usize = 17;
+const SYS_pipe2: usize = 59;
+const SYS_dup: usize = 23;
+const SYS_dup3: usize = 24;
+const SYS_chdir: usize = 49;
+const SYS_openat: usize = 56;
+const SYS_close: usize = 57;
+const SYS_getdents64: usize = 61;
+const SYS_read: usize = 63;
+const SYS_write: usize = 64;
+const SYS_linkat: usize = 37;
+const SYS_unlinkat: usize = 35;
+const SYS_mkdirat: usize = 34;
+const SYS_umount2: usize = 39;
+const SYS_mount: usize = 40;
+const SYS_fstat: usize = 80;
+
+const SYS_clone: usize = 220;
+const SYS_execve: usize = 221;
+const SYS_wait4: usize = 260;
+const SYS_exit: usize = 93;
+const SYS_getppid: usize = 173;
+const SYS_getpid: usize = 172;
+
+const SYS_brk: usize = 214;
+const SYS_munmap: usize = 215;
+const SYS_mmap: usize = 222;
+const SYS_times: usize = 153;
+const SYS_uname: usize = 160;
+const SYS_sched_yield: usize = 124;
+const SYS_gettimeofday: usize = 169;
+const SYS_nanosleep: usize = 101;
 
 mod fs;
 mod process;
@@ -42,6 +80,9 @@ pub async fn syscall(syscall_id: usize, args: [usize; 3]) -> SyscallRet {
         SYSCALL_FORK => sys_fork(),
         SYSCALL_EXEC => sys_exec(args[0]).await,
         SYSCALL_WAITPID => sys_waitpid(args[0] as isize, args[1] as *mut i32),
+
+        SYS_getcwd => sys_getcwd(args[0], args[1]),
+        // SYS_chdir => sys_chdir(args[0] as *const u8),
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
     }
 }
